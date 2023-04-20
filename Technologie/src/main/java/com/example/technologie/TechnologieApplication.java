3d99d4fad420ebc7e38d057147ad31bad4805152
Merman1 <85934727+Merman1@users.noreply.github.com>
2023-04-20 23:01:41 +0200

    Add files via upload
    oki

---
+++ Technologie/src/main/java/com/example/technologie/TechnologieApplication.java
@@ -0,0 +1,13 @@
+package com.example.technologie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologieApplication.class, args);
+    }
+
+}
---
+++ Technologie/src/test/java/com/example/technologie/TechnologieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technologie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
